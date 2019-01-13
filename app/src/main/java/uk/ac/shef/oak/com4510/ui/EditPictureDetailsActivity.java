3ce5a4af791b26e8f243c9f4d0be801089869860
unknown <fernando.montero.perez@gmail.com>
2019-01-13 14:13:31 +0000

    all workingç

--- app/src/main/java/uk/ac/shef/oak/com4510/ui/EditPictureDetailsActivity.java
+++ app/src/main/java/uk/ac/shef/oak/com4510/ui/EditPictureDetailsActivity.java
@@ -46,7 +46,7 @@ public class EditPictureDetailsActivity extends AppCompatActivity {
         extras = getIntent().getExtras();
         String imagePath = extras.getString("ImagePath");
 //        Log.d("ImageRe - Intent", imagePath);
-        ActivitySinglePictureBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_picture_details);
+        ActivityEditPictureDetailsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_picture_details);
 
         model = ViewModelProviders.of(this).get(SinglePictureViewModel.class);
 
